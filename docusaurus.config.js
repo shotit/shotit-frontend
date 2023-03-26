b95b0024b9b8afd596db8f32242442e6532cb8a9
Leslie Wong <79917148leslie@gmail.com>
2023-03-26 17:11:05 +0800

    Change brand information

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,7 +6,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
+  title: 'Shotit',
   tagline: 'Dinosaurs are cool',
   favicon: 'img/favicon.ico',
 
@@ -64,9 +64,9 @@ const config = {
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
       navbar: {
-        title: 'My Site',
+        title: 'Shotit',
         logo: {
-          alt: 'My Site Logo',
+          alt: 'Shotit Logo',
           src: 'img/logo.svg',
         },
         items: [
@@ -127,7 +127,7 @@ const config = {
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        copyright: `Copyright © ${new Date().getFullYear()} PolarBeaver Technology. Built with Docusaurus.`,
       },
       prism: {
         theme: lightCodeTheme,
