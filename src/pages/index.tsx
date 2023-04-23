146437874e35f03538fbd090426c6c53391abeb0
Leslie Wong <79917148leslie@gmail.com>
2023-04-24 02:25:46 +0800

    Draft home page cover

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,22 +4,39 @@ import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
-
+import Logo from '@site/static/img/logo.svg';
+import LogoBg from '@site/static/img/logo-bg.svg';
 import styles from './index.module.css';
 
 function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
-        <h1 className="hero__title">{siteConfig.title}</h1>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
+        <h1 className="hero__title">Screenshot-to-Video</h1>
+        <h1 className="hero__title">Search Engine</h1>
+        <p className="hero__subtitle">
+          <div>Shotit is a screenshot-to-video search engine</div>
+          <div>tailored for TV & Film, blazing-fast and</div>
+          <div>compute-efficient.</div>
+        </p>
+        <div className={styles.btnGroup}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
+            to="/docs/intro"
+          >
+            Get Started
           </Link>
+          <Link className="button button--success button--lg" to="/docs/intro">
+            Try a Demo
+          </Link>
+        </div>
+      </div>
+      <div className={styles.logoBgContainer}>
+        <div className={styles.logoBg}>
+          <LogoBg height={563} width={666} className={styles.svgContent} />
+        </div>
+        <div className={styles.logo}>
+          <Logo height={600} width={600} className={styles.svgContent} />
         </div>
       </div>
     </header>
@@ -27,11 +44,12 @@ function HomepageHeader() {
 }
 
 export default function Home(): JSX.Element {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      title={`Shotit, ${siteConfig.title}`}
+      description="Shotit is a screenshot-to-video Search Engine tailored for TV & Film, blazing-fast and compute-efficient."
+    >
       <HomepageHeader />
       <main>
         <HomepageFeatures />
